029c59f7afa10227206b80510ceb36bb7c04a872
DiogoBurgierman <124000765+DiogoBurgierman@users.noreply.github.com>
2023-03-15 22:12:55 -0300

    v1

--- SEMANA_07/02_ATIVIDADES/ExSemana7/cenaMesa.gd
+++ SEMANA_07/02_ATIVIDADES/ExSemana7/cenaMesa.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+var cursorVazio = load("res://imagens/dropper pixelart.png")
+var cursorCheio = load("res://imagens/pixil-frame-0 (6).png")
+var multiplicador = 2
 
+func _ready():
+	Input.set_custom_mouse_cursor(cursorVazio)
 
 func _on_Penicillium_pressed():
 	get_tree().change_scene("res://cenaFungo.tscn")
